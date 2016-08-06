9412352d2c90e371362fbd6e3c31ef9c409fefeb
Erik Schoster <erik@hecanjog.com>
2016-08-05 22:27:29 -0500

    Add release metadata

--- catalog/models.py
+++ catalog/models.py
@@ -5,6 +5,16 @@ from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
 import datetime
 
+RELEASE_FORMATS = [
+    ('cd', 'Compact Disc'),
+    ('cdr', 'Compact Disc-Recordable'),
+    ('digital', 'Digital'),
+    ('usb', 'USB / Flash Media'),
+    ('vinyl', 'Vinyl Record'),
+    ('cassette', 'Cassette Tape'),
+    ('other', 'Other'),
+]
+
 @python_2_unicode_compatible
 class Band(models.Model):
     name = models.CharField(max_length=255)
@@ -41,11 +51,15 @@ class Label(models.Model):
 class Release(models.Model):
     title = models.CharField(max_length=255)
     cover = models.ImageField(upload_to='uploads/%Y/%m/%d/', null=True, blank=True)
+    catalog_id = models.CharField(max_length=255, null=True, blank=True, unique=True)
+    format = models.CharField(max_length=255, choices=RELEASE_FORMATS, default='cd')
     published = models.BooleanField(default=False)
     published_date = models.DateField(default=timezone.now, null=True, blank=True)
     artist = models.ForeignKey('Artist', on_delete=models.DO_NOTHING, null=True, blank=True)
     band = models.ForeignKey('Band', on_delete=models.DO_NOTHING, null=True, blank=True)
     label = models.ForeignKey('Label', on_delete=models.DO_NOTHING, null=True, blank=True)
+    notes = models.TextField(null=True, blank=True)
+    credits = models.TextField(null=True, blank=True)
     created = models.DateTimeField(default=timezone.now)
     updated = models.DateTimeField(default=timezone.now)
     deleted = models.DateTimeField(null=True, blank=True)
@@ -63,6 +77,20 @@ class Track(models.Model):
     deleted = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
-        return '{} - {}'.format(self.id, self.name)
+        return self.name
+
+@python_2_unicode_compatible
+class Review(models.Model):
+    release = models.ForeignKey('Release', on_delete=models.DO_NOTHING, null=True, blank=True)
+    publication_name = models.CharField(max_length=255)
+    publication_date = models.DateField(default=timezone.now, null=True, blank=True)
+    url = models.CharField(max_length=255, null=True, blank=True)
+    review = models.TextField()
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(default=timezone.now)
+    deleted = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
